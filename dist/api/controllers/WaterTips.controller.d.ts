64f666724b6f3b125f6109d4b77d28a6baf0b13f
Boda <145921068+Boda01099@users.noreply.github.com>
2025-07-31 03:58:39 +0300

    Delete dist directory

--- dist/api/controllers/WaterTips.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare class WaterTips {
-    static GetTip(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-    static AddTip(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-    static RemoveTip(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-    static ListTip(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-}
--- dist/api/middlewares/error.middleware.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-interface ErrorWithStatusCode extends Error {
-    statusCode?: number;
-}
-declare const errorMiddleware: (err: ErrorWithStatusCode, req: Request, res: Response, next: NextFunction) => void;
-export default errorMiddleware;
